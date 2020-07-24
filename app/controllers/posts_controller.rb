6106ce6dbda74193fbed256ec6de6fce07c7f3b6
21shield <21netaly@gmail.com>
2020-07-24 15:46:14 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
@@ -8,9 +9,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    #  binding.pry
+    @post.assign_attributes(post_params)
+    if @post.save
+     redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
